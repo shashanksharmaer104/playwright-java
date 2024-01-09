ef426a6e18d6842731ff57eec3e72b60450e24f1
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-01-09 22:12:20 +0530

    first commit

---
+++ src/test/java/com/automation/day2/LaunchBrowser.java
@@ -0,0 +1,35 @@
+package com.automation.day2;
+
+import com.microsoft.playwright.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+
+        Page page = browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/");
+
+        Locator myAccount = page.locator("//a[contains(.,' My account')][@role='button']");
+        myAccount.hover();
+
+        Locator login = page.locator("//a[contains(.,'Login')][1]");
+        login.click();
+
+        assertThat(page).hasTitle("Account Login");
+
+        page.getByPlaceholder("E-Mail Address").type("shashanksharma@gmail.com");
+        page.getByPlaceholder("Password").type("shashanksharma");
+        page.locator("//*[@value='Login']").click();
+
+        assertThat(page).hasTitle("My Account");
+
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
