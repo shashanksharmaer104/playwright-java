6d8a2192c3938f973a8732cb7a85fedbfe905099
Shashank Sharma <shasharm18@WKMZT7B031E9.global.publicisgroupe.net>
2024-01-11 17:31:53 +0530

    Added steps to interact with various inputs

--- src/test/java/com/automation/day2/InteractWithInputs.java
+++ src/test/java/com/automation/day2/InteractWithInputs.java
@@ -1,9 +1,6 @@
 package com.automation.day2;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -12,11 +9,11 @@ public class InteractWithInputs {
 
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(false).setChannel("msedge")
         );
         Page page = browser.newPage();
 
-        page.navigate("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+        /*page.navigate("https://www.lambdatest.com/selenium-playground/simple-form-demo");
         page.locator("input#user-message").type("Hey user");
         page.click("#showInput");
 
@@ -24,6 +21,35 @@ public class InteractWithInputs {
         System.out.println(message);
 
         assertThat(page.locator("#message")).hasText("Hey user");
+
+        // type vs fill
+        page.navigate("https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo");
+        //page.locator("#textbox").type("Data entered in the below textarea will be download with file name 'Lambdainfo.txt'.");
+        page.locator("#textbox").fill("Data entered in the below textarea will be download with file name 'Lambdainfo.txt'.");*/
+
+        // Get input value of textbox from UI
+        page.navigate("https://letcode.in/edit");
+        String value = page.locator("#getMe").inputValue();
+        System.out.println(value);
+
+        // Get placeholder value of textbox from UI
+        Locator locator = page.locator("#fullName");
+        System.out.println("Placeholder value: " + locator.getAttribute("placeholder"));
+        assertThat(locator).hasAttribute("placeholder", "Enter first & last name");
+
+        // Get text of label from UI
+        Locator label = page.locator("(//label[@for='name'])[1]");
+        System.out.println("Label text: " + label.textContent());
+
+        // Clear textbox value from UI
+        page.locator("#clearMe").clear();
+
+        // Verify checbox assertion for check and uncheck functionality
+        page.navigate("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+        Locator checkbox = page.locator("#isAgeSelected");
+        assertThat(checkbox).not().isChecked();
+        checkbox.check(); // click on checkbox
+        assertThat(checkbox).isChecked();
 
         playwright.close();
     }
